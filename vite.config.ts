517802ea7a11e69bd5bf86fcb8fa32f8b2b4c258
Thanakrit-Anutrakulchai <35787417+Thanakrit-Anutrakulchai@users.noreply.github.com>
2024-10-25 19:31:27 +0700

    Add legacy browser support
    Via @vitejs/plugin-legacy. I don't have good ways of testing this beyond
    an old macbook pro 2017, unfortunately.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        legacy({
+            targets: ['defaults', 'safari >= 12', 'iOS >= 12', 'chrome >= 60', 'firefox >= 55'],
+        })
+    ],
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
             }
-        }
+        },
     }
 })
